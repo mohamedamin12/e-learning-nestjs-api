2ac978c4ea749f477350b4253eb8fa52b3284a63
mohamedamin12 <aboamin909080@gmail.com>
2025-04-11 06:03:13 +0200

    feat: enhance security and validation in the application
    - Added new dependencies for security and validation: @nestjs/jwt, @nestjs/passport, @nestjs/swagger, @nestjs/typeorm, bcrypt, class-transformer, class-validator, cloudinary, compression, cookie-parser, csurf, dotenv, express-rate-limit, helmet, hpp, multer, nodemailer, pg, sanitize-html, slugify, streamifier, typeorm.
    - Implemented middleware for security features including helmet, rate limiting, CORS, HTTP parameter pollution prevention, and response compression in main.ts.
    - Introduced SanitizeHtmlPipe for sanitizing input data to prevent XSS attacks.
    - Removed unused AppController test file to clean up the codebase.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,62 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import * as compression from 'compression';
+import rateLimit from 'express-rate-limit';
+import * as csurf from 'csurf';
+import * as cookieParser from 'cookie-parser';
+import * as express from 'express';
+import * as hpp from 'hpp';
+import helmet from 'helmet';
+import { SanitizeHtmlPipe } from './utils/sanitizeHtml-pipe';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 5000);
+
+  app.use(helmet());
+
+  app.use(cookieParser());
+
+  app.use(express.json({ limit: '10kb' }));
+
+  // Rate Limiting to prevent DOS attacks
+  app.use(
+    rateLimit({
+      windowMs: 10 * 60 * 1000, // 10 minutes
+      max: 50, // limit each IP to 50 requests per windowMs
+      message: 'You have exceeded the 50 requests in 10 minutes limit!',
+      headers: true,
+      handler: (req, res) => {
+        res.status(429).json({
+          status: 'error',
+          message: 'Too many requests, please try again later.',
+        });
+      },
+    }),
+  );
+
+  // Data Validation and Sanitization
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  app.useGlobalPipes(new SanitizeHtmlPipe());
+
+  // Enable Cross-Origin Resource Sharing (CORS)
+  app.enableCors();
+
+  // HTTP Parameter Pollution Prevention
+  app.use(hpp({}));
+
+  // Response Compression for better performance
+  app.use(compression());
+
+
+  await app.listen(5000);
 }
 bootstrap();
